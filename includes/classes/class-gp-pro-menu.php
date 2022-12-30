77a4a635eaa48f6c8f0f934433c76a0cf8919112
arabobo0109 <tomodachi0109@outlook.com>
2022-12-31 02:01:58 +0800

    Menu objects for GP Premium

---
+++ includes/classes/class-gp-pro-menu.php
@@ -0,0 +1,64 @@
+<?php
+
+class GP_Pro_Menu
+{
+  public static function elements_menu()
+  {
+    return array(
+      'parent'=> 'gp-shortcuts-generatepress',
+      'id'    => 'gp-shortcuts-elements-menu',
+      'title' => __( 'Add New Elements', 'gp-shortcuts' ),
+      'href'  => admin_url( "edit.php?post_type=gp_elements" ),
+    );
+  }
+
+  public static function site_library()
+  {
+    return array(
+      'parent'=> 'gp-shortcuts-generatepress',
+      'id'    => 'gp-shortcuts-site-library',
+      'title' => __( 'Site Library', 'gp-shortcuts' ),
+      'href'  => admin_url( "themes.php?page=generatepress-library" ),
+    );
+  }
+
+  public static function block()
+  {
+    return array(
+      'parent'=> 'gp-shortcuts-elements-menu',
+      'id'    => 'gp-shortcuts-elements-block',
+      'title' => __( 'Block', 'gp-shortcuts' ),
+      'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=block" ),
+    );
+  }
+
+  public static function hook()
+  {
+    return array(
+      'parent'=> 'gp-shortcuts-elements-menu',
+      'id'    => 'gp-shortcuts-elements-hook',
+      'title' => __( 'Hook', 'gp-shortcuts' ),
+      'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=hook" ),
+    );
+  }
+
+  public static function layout()
+  {
+    return array(
+      'parent'=> 'gp-shortcuts-elements-menu',
+      'id'    => 'gp-shortcuts-elements-layout',
+      'title' => __( 'Layout', 'gp-shortcuts' ),
+      'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=layout" ),
+    );
+  }
+
+  public static function header()
+  {
+    return array(
+      'parent'=> 'gp-shortcuts-elements-menu',
+      'id'    => 'gp-shortcuts-elements-header',
+      'title' => __( 'Header', 'gp-shortcuts' ),
+      'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=header" ),
+    );
+  }
+}
