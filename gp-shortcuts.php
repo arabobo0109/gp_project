9fc0b16ca68249d28ddd28539e8d2d7a5fd2e1a0
arabobo0109 <tomodachi0109@outlook.com>
2023-01-04 00:46:58 +0800

    `v1.0.1`

--- gp-shortcuts.php
+++ gp-shortcuts.php
@@ -10,7 +10,7 @@
  * Plugin Name:       GP Shortcuts
  * Plugin URI:        https://github.com/alvindcaesar/gp-shortcuts
  * Description:       Adds a menu to the admin bar to easily jump straight into GeneratePress and GenerateBlocks menus and settings.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Alvind Caesar
  * Author URI:        https://alvindcaesar.com
  * License:           GPL-2.0+
