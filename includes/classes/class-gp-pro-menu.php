1a2ecf0a86fc02ee99aab4352ce5b0a574d96f54
arabobo0109 <tomodachi0109@outlook.com>
2023-01-04 00:46:28 +0800

    Separate `Elements` and `Add New Elements` menu

--- includes/classes/class-gp-pro-menu.php
+++ includes/classes/class-gp-pro-menu.php
@@ -7,8 +7,18 @@ class GP_Pro_Menu
     return array(
       'parent'=> 'gp-shortcuts-generatepress',
       'id'    => 'gp-shortcuts-elements-menu',
-      'title' => __( 'Add New Elements', 'gp-shortcuts' ),
+      'title' => __( 'Elements', 'gp-shortcuts' ),
       'href'  => admin_url( "edit.php?post_type=gp_elements" ),
+    );
+  }
+
+  public static function add_elements_menu()
+  {
+    return array(
+      'parent'=> 'gp-shortcuts-generatepress',
+      'id'    => 'gp-shortcuts-add-elements-menu',
+      'title' => __( 'Add New Elements', 'gp-shortcuts' ),
+      'href'  => '#',
     );
   }
 
@@ -25,7 +35,7 @@ class GP_Pro_Menu
   public static function block()
   {
     return array(
-      'parent'=> 'gp-shortcuts-elements-menu',
+      'parent'=> 'gp-shortcuts-add-elements-menu',
       'id'    => 'gp-shortcuts-elements-block',
       'title' => __( 'Block', 'gp-shortcuts' ),
       'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=block" ),
@@ -35,7 +45,7 @@ class GP_Pro_Menu
   public static function hook()
   {
     return array(
-      'parent'=> 'gp-shortcuts-elements-menu',
+      'parent'=> 'gp-shortcuts-add-elements-menu',
       'id'    => 'gp-shortcuts-elements-hook',
       'title' => __( 'Hook', 'gp-shortcuts' ),
       'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=hook" ),
@@ -45,7 +55,7 @@ class GP_Pro_Menu
   public static function layout()
   {
     return array(
-      'parent'=> 'gp-shortcuts-elements-menu',
+      'parent'=> 'gp-shortcuts-add-elements-menu',
       'id'    => 'gp-shortcuts-elements-layout',
       'title' => __( 'Layout', 'gp-shortcuts' ),
       'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=layout" ),
@@ -55,7 +65,7 @@ class GP_Pro_Menu
   public static function header()
   {
     return array(
-      'parent'=> 'gp-shortcuts-elements-menu',
+      'parent'=> 'gp-shortcuts-add-elements-menu',
       'id'    => 'gp-shortcuts-elements-header',
       'title' => __( 'Header', 'gp-shortcuts' ),
       'href'  => admin_url( "post-new.php?post_type=gp_elements&element_type=header" ),
